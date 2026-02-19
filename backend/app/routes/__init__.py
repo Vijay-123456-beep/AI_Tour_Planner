194c6acc575b45c29bdfd5366fc31c20dad5f15b
Vijay Kavuri <vijaykavuri67@gmail.com>
2026-02-19 13:26:15 +0530

    Fix deployment: Remove gevent, add Procfile

--- backend/app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Routes package initialization
-"""
-from .auth import auth_bp
-from .itinerary import itinerary_bp
-from .collaboration import collaboration_bp
-
-__all__ = ['auth_bp', 'itinerary_bp', 'collaboration_bp']
